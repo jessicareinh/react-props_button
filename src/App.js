2cbd241479caa776e2dafb36c4a524903ddbeca8
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2024-03-04 19:24:54 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
 import "./styles.css";
 
+
+function Button({ color, disabled, text }) {
+  const buttonStyle = {
+    color: color,
+    backgroundColor: "rebeccapurple",
+    padding: "10px 20px",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer"
+  };
+
+  return (
+    <button style={buttonStyle} disabled={disabled}>{text}</button>
+  );
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div className="App">
+      <Button color="white" disabled={false} text="Click me" />
+    </div>
+  );
 }
